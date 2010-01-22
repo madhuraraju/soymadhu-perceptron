879bfecf14b177b4e070b35bfc4a5a57f67946d4
Leif Johnson <leif@leifjohnson.net>
2010-01-21 20:08:19 -0600

    Clean up the code significantly. Pull out the test into a separate script, and change it to use a dataset that is used for other ML tests. Change test output accordingly.

--- perceptron.py
+++ perceptron.py
@@ -20,89 +20,49 @@
 
 '''Basic Perceptron library.
 
-Perceptrons are supervised, discriminative models for classifying data points
-(also called "events") into one of a number of discrete categories (also called
-"outcomes"). The basic Perceptron is simply a vector W in event space that is
-normal to a linear separating surface going through the origin. Data points that
-fall on one side of the surface are in outcome (+), and data points on the other
-side are in outcome (-). To determine the side of the surface on which some
-event X falls, we compute the dot product of X and W and return the sign of the
-resulting scalar. Learning takes place in the Perceptron whenever it makes an
-incorrect classification from labeled data : X is simply added to W to minimally
-correct the output of the Perceptron for this data point. This might invalidate
+Perceptrons are discriminative models for classifying data points (also called
+"events") into one of a number of discrete categories (also called "outcomes").
+The basic Perceptron is simply a vector W in event space that is normal to a
+linear separating surface going through the origin. Data points that fall on one
+side of the surface are in outcome (+), and data points on the other side are in
+outcome (-). To determine the side of the surface on which some event X falls,
+we compute the dot product of X and W and return the sign of the resulting
+scalar. Learning takes place in the Perceptron whenever it makes an incorrect
+classification from labeled data : X is simply added to W to minimally correct
+the output of the Perceptron for this data point. This might invalidate
 classifications for other events in the data set, but it is simple.
 
 This particular library generalizes the Perceptron to K > 2 outcomes by
-maintaining a surface for each of the K possible outcomes. Each surface can be
-thought of as separating events in one outcome from events in all other
-outcomes. At classification time, we compute the dot product of X with each of
+maintaining a decision surface for each of the K possible outcomes. Each surface
+can be thought of as separating events in one outcome from events in all other
+outcomes. At classification time, we compute the kernel value of X and each of
 the Ws. The outcome with the greatest resulting scalar value is returned as the
-output.
+prediction for that event.
 
 This library also incorporates the "kernel trick" to generalize the notion of
-the dot product to (almost) arbitrary high-dimensional spaces. Instead of
-computing the dot product between X and W, we use a Kernel function that accepts
-X and W as inputs and returns a scalar value indicating their similarity in some
-(often much higher-dimensional) mapped comparison space.
-
-Run a test on the library by calling this script from the command line.
+the dot product to spaces of different dimensionality. Instead of computing the
+dot product between X and W, we use a Kernel function that accepts X and W as
+inputs and returns a scalar value indicating their similarity in some mapped
+comparison space.
 '''
 
 import numpy
-from numpy import random as rng
+from collections import defaultdict
 
 
-class Kernel(object):
-    '''A Kernel function maps two vectors onto a scalar.
-
-    Kernels must be decomposable into the product of some function mapped to
-    each of the input vectors.
-    '''
-
-    def __call__(self, a, b):
-        '''Obtain a scalar from two vectors.'''
-        raise NotImplemented
+def radial_basis_kernel(gamma):
+    '''This kernel returns the rbf between the input vectors.'''
+    def run(weights, event):
+        delta = numpy.linalg.norm(event - weights)
+        return numpy.exp(-gamma * delta * delta)
+    return run
 
 
-class DotProductKernel(Kernel):
-    '''This Kernel calculates the standard dot product of two vectors.'''
-
-    def __call__(self, a, b):
-        return numpy.dot(a, b)
-
-
-class RadialBasisKernel(Kernel):
-    '''This Kernel returns the gaussian distance between the input vectors.'''
-
-    def __init__(self, gamma):
-        self._gamma = gamma
-
-    def __call__(self, a, b):
-        delta = b - a
-        return numpy.exp(-self._gamma * (delta * delta).sum())
-
-
-class PolynomialKernel(Kernel):
-    '''This Kernel returns the dot product raised to some power.'''
-
-    def __init__(self, degree, alpha=1.0):
-        self._degree = degree
-        self._alpha = alpha
-
-    def __call__(self, a, b):
-        return (numpy.dot(a, b) + self._alpha) ** self._degree
-
-
-def _max_outcome(weights, kernel, event):
-    '''Return the maximum scoring outcome for a set of weights.'''
-    max_outcome = -1
-    max_score = -numpy.inf
-    for outcome in xrange(weights.shape[0]):
-        score = kernel(weights[outcome, :], event)
-        if score > max_score:
-            max_outcome = outcome
-            max_score = score
-    return max_outcome, max_score
+def polynomial_kernel(degree, alpha=1.0):
+    '''This kernel returns the dot product raised to some power.'''
+    def run(weights, event):
+        return (numpy.dot(weights, event) + alpha) ** degree
+    return run
 
 
 class Perceptron(object):
@@ -118,23 +78,17 @@ class Perceptron(object):
         '''
         assert outcome_size >= 2
         assert event_size >= 1
-        self._weights = numpy.zeros((outcome_size, event_size), dtype='d')
-        self._kernel = kernel or DotProductKernel()
-
-    def _update(self, outcome, target):
-        '''Update the weights for a single outcome toward a particular target.'''
-        W = self._weights[outcome, :]
-        W += target
-        W /= numpy.sqrt((W * W).sum())
+        self._weights = numpy.zeros((outcome_size, event_size), 'd')
+        self._kernel = kernel or numpy.dot
 
     def classify(self, event):
         '''Classify an event into one of our possible outcomes.
 
         event: A numpy vector of the dimensionality of our input space.
 
-        Returns the most likely outcome, an integer in [0, outcome_size).
+        Returns a pair of (outcome, score) for the most likely outcome.
         '''
-        return _max_outcome(self._weights, self._kernel, event)
+        return Perceptron._max_outcome(self._weights, event, self._kernel)
 
     def learn(self, event, outcome):
         '''Adjust the hyperplane based on a classification attempt.
@@ -143,10 +97,36 @@ class Perceptron(object):
         outcome: An integer in [0, outcome_size) indicating the correct outcome
           for this event.
         '''
-        prediction, _ = self.classify(event)
-        if prediction is not outcome:
-            self._update(prediction, -event)
-            self._update(outcome, event)
+        pred, _ = self.classify(event)
+        if pred != outcome:
+            self._update_weights(pred, event, -1)
+            self._update_weights(outcome, event, 1)
+
+    def _update_weights(self, class_index, event, delta):
+        '''Update the weights for an index based on an event.
+
+        class_index: The index of a weight vector to update.
+        event: An event vector to use for the update.
+        delta: Weight the update by this value.
+        '''
+        self._weights[class_index] += delta * event
+
+    @staticmethod
+    def _max_outcome(weights, event, kernel):
+        '''Return the maximum scoring outcome for a set of weights.
+
+        weights: An array of weight vectors, one per outcome.
+        event: An event vector.
+        kernel: A kernel function to combine weights with the event.
+        '''
+        max_outcome = 0
+        max_score = -1e100
+        for o, w in enumerate(weights):
+            score = kernel(w, event)
+            if score > max_score:
+                max_outcome = o
+                max_score = score
+        return max_outcome, max_score
 
 
 class AveragedPerceptron(Perceptron):
@@ -173,75 +153,91 @@ class AveragedPerceptron(Perceptron):
     '''
 
     def __init__(self, event_size, outcome_size, kernel=None):
-        parent = super(AveragedPerceptron, self)
-        parent.__init__(event_size, outcome_size, kernel)
+        super(AveragedPerceptron, self).__init__(
+            event_size, outcome_size, kernel)
         self._iterations = 0
         self._survived = 0
-        self._history = self._weights.copy()
-
-    def learn(self, event, outcome):
-        self._iterations += 1
-        self._survived += 1
-        prediction, _ = _max_outcome(self._weights, self._kernel, event)
-        if outcome is not prediction:
-            self._history += self._weights * self._survived
-            self._survived = 0
-            self._update(prediction, -event)
-            self._update(outcome, event)
+        self._history = numpy.zeros(self._weights.shape, 'd')
 
     def classify(self, event):
-        weights = self._history + self._weights * self._survived
-        return _max_outcome(weights / self._iterations, self._kernel, event)
+        '''Classify an event into one of our possible outcomes.
+
+        event: A numpy vector of the dimensionality of our input space.
+
+        Returns a pair of (outcome, score) for the most likely outcome.
+        '''
+        o, s = Perceptron._max_outcome(self._history, event, self._kernel)
+        return o, s / self._iterations
+
+    def learn(self, event, outcome):
+        '''Adjust the hyperplane based on a classification attempt.
+
+        event: A numpy vector of the dimensionality of our input space.
+        outcome: An integer indicating the correct outcome for this event.
+        '''
+        pred, _ = Perceptron._max_outcome(self._weights, event, self._kernel)
+        if pred != outcome:
+            if self._survived > 0:
+                self._update_history(pred)
+                self._update_history(outcome)
+                self._survived = 0
+            self._update_weights(pred, event, -1)
+            self._update_weights(outcome, event, 1)
+        else:
+            self._survived += 1
+        self._iterations += 1
+
+    def _update_history(self, class_index):
+        '''Update the history for a particular class.'''
+        self._history[class_index] += \
+            self._survived * self._weights[class_index]
 
 
-if __name__ == '__main__':
-    centers = (
-        (0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1),
-        (0, -1,  1, -1,  1, -1,  1, -1,  1, -1,  1, -1,  1, -1,  1, -1,  1),
-        (0, -1, -1,  1,  1, -1, -1,  1,  1, -1, -1,  1,  1, -1, -1,  1,  1),
-        (0, -1, -1, -1, -1,  1,  1,  1,  1, -1, -1, -1, -1,  1,  1,  1,  1),
-        (0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1),
-        )
+class SparseAveragedPerceptron(AveragedPerceptron):
+    '''A voted perceptron using sparse vectors for storing weights.
 
-    def sample(variance):
-        outcome = int(rng.random() * len(centers))
-        event = rng.normal(centers[outcome], variance)
-        event[0] = 1  # always set the bias to 1.
-        return event / numpy.sqrt((event * event).sum()), outcome
+    This class achieves sparseness by assuming that all events are
+    binary-valued. To use this class with a non-binary event space, you will
+    need to manually chop up the event space into binary events. For example,
+    suppose event 'foo' can take on N possible discrete values ; you might
+    convert 'foo' into N separate events, 'foo=1' .. 'foo=N'.
 
-    kwargs = dict(event_size=len(centers[0]), outcome_size=len(centers))
+    In addition, the classifier only retains the top-weighted features for
+    each class, making this a sort of beam search version of the general
+    Perceptron. This reduces the space requirements for the classifier.
+    '''
 
-    learners = []
-    for Klass in (Perceptron, AveragedPerceptron):
-        learners.append(Klass(**kwargs))
-        for d in (1, 2, 5):
-            learners.append(Klass(kernel=PolynomialKernel(d), **kwargs))
-        learners.append(Klass(kernel=RadialBasisKernel(0.5), **kwargs))
+    def __init__(self, event_size, outcome_size, beam_width=1000):
+        '''Use sparse vectors to store weights and events.'''
+        super(SparseAveragedPerceptron, self).__init__(event_size, outcome_size)
+        self._weights = [defaultdict(float) for _ in xrange(outcome_size)]
+        self._history = [defaultdict(float) for _ in xrange(outcome_size)]
+        self._kernel = SparseAveragedPerceptron._dot
+        self._beam_width = beam_width
 
-    print 'training data: 500 points drawn randomly from %d gaussians' % len(centers)
-    print '% correct on 100 points drawn from the same gaussians'
-    print 'Var\t| P\tP:p1\tP:p2\tP:p5\tP:r\t| A\tA:p1\tA:p2\tA:p5\tA:r'
+    @staticmethod
+    def _dot(weights, event):
+        '''Return the dot product of a sparse vector and an event.'''
+        return sum(weights.get(f, 0) for f in event)
 
-    for variance in (0.1, 0.2, 0.5, 1, 2, 5, 10):
-        # train all classifiers on a set of sample data.
-        for _ in xrange(500):
-            event, outcome = sample(variance)
-            for p in learners:
-                p.learn(event, outcome)
+    def _update_history(self, class_index):
+        '''Update the history for a particular class.'''
+        h = self._history[class_index]
+        for f, w in self._weights[class_index].iteritems():
+            h[f] += self._survived * w
+        self._prune(h)
 
-        # test on a different set of data.
-        correct = [0] * len(learners)
-        for _ in xrange(100):
-            event, outcome = sample(variance)
-            for i, p in enumerate(learners):
-                prediction, _ = p.classify(event)
-                if prediction is outcome:
-                    correct[i] += 1
+    def _update_weights(self, class_index, event, delta):
+        '''Update the weights for an index based on an event.'''
+        w = self._weights[class_index]
+        for f in event:
+            w[f] += delta
+        self._prune(w)
 
-        # display some results.
-        print '%4.1f' % variance, '\t|',
-        for i, p in enumerate(learners):
-            print '%3d' % correct[i], '\t',
-            if i == 4:
-                print '|',
-        print
+    def _prune(self, weights):
+        '''Prune the weights in a sparse vector to our beam width.'''
+        if len(weights) < 1.5 * self._beam_width:
+            return
+        fws = sorted(weights.iteritems(), key=lambda x: -abs(x[1]))
+        for f, _ in fws[self._beam_width:]:
+            del weights[f]
